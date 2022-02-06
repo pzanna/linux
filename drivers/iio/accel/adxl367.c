c23a702c616b48f013074e867b1426809e9c204f
Cosmin Tanislav <cosmin.tanislav@analog.com>
2022-02-06 20:35:55 +0200

    iio: accel: adxl367: IIO_EV_TYPE_THRESH -> IIO_EV_TYPE_MAG_REFERENCED

--- drivers/iio/accel/adxl367.c
+++ drivers/iio/accel/adxl367.c
@@ -1387,14 +1387,14 @@ static const struct iio_info adxl367_info = {
 
 static const struct iio_event_spec adxl367_events[] = {
 	{
-		.type = IIO_EV_TYPE_THRESH,
+		.type = IIO_EV_TYPE_MAG_REFERENCED,
 		.dir = IIO_EV_DIR_RISING,
 		.mask_shared_by_all = BIT(IIO_EV_INFO_ENABLE) |
 				      BIT(IIO_EV_INFO_PERIOD) |
 				      BIT(IIO_EV_INFO_VALUE),
 	},
 	{
-		.type = IIO_EV_TYPE_THRESH,
+		.type = IIO_EV_TYPE_MAG_REFERENCED,
 		.dir = IIO_EV_DIR_FALLING,
 		.mask_shared_by_all = BIT(IIO_EV_INFO_ENABLE) |
 				      BIT(IIO_EV_INFO_PERIOD) |
